e8526f8db06984cf8a97f8ea37947e764f7248e8
Artiru <gignt0646@mail.ru>
2025-03-01 20:24:28 +0500

    feat(basket): add commitlint config

---
+++ commitizen.config.js
@@ -0,0 +1,8 @@
+const custom = require('@digitalroute/cz-conventional-changelog-for-jira/configurable');
+const defaultTypes = require('@digitalroute/cz-conventional-changelog-for-jira/types');
+
+module.exports = custom({
+  types: defaultTypes,
+  skipScope: false,
+  jiraPrefix: 'PAO',
+});
